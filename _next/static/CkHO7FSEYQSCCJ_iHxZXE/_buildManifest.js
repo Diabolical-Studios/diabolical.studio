64e6fc60a88106bbdbebbb0f3dc49afde83b4427
blazittx <info@doruksasmaz.com>
2024-02-17 15:30:49 +0300

    Diabolical Studios Development Branch

---
+++ _next/static/CkHO7FSEYQSCCJ_iHxZXE/_buildManifest.js
@@ -0,0 +1,15 @@
+self.__BUILD_MANIFEST = function(s, c, e, a) {
+    return {
+        __rewrites: {
+            beforeFiles: [],
+            afterFiles: [],
+            fallback: []
+        },
+        "/": [s, c, e, a, "static/css/5d291cb8cf98045b.css", "static/chunks/pages/index-6222560f84639de6.js"],
+        "/_error": ["static/chunks/pages/_error-7f1f326cc5f96728.js"],
+        "/about": [s, e, "static/css/15b79221d34ad0a9.css", "static/chunks/pages/about-5ea73d67ea68df3b.js"],
+        "/music": [s, c, a, "static/css/1b177cf53900df51.css", "static/chunks/pages/music-2b452eb3a459e6a2.js"],
+        sortedPages: ["/", "/_app", "/_error", "/about", "/music"]
+    }
+}("static/chunks/152-76563cd112a12f61.js", "static/chunks/928-a615bd35cb987531.js", "static/chunks/415-736b671a0373d520.js", "static/chunks/416-ac30306b5a08c747.js"),
+self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB();
