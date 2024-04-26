a7f70c9a7deb612103500062a99df4738fb108e6
blazittx <info@doruksasmaz.com>
2024-04-26 07:39:05 +0300

    Update checkUsername.js

--- netlify/functions/checkUsername.js
+++ netlify/functions/checkUsername.js
@@ -1,31 +1,38 @@
 exports.handler = async (event) => {
-    if (event.httpMethod !== 'GET') { // Ensure it's a GET request
-        return {
-            statusCode: 405,
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ message: "Method Not Allowed" })
-        };
-    }
-
-    const playerName = event.pathParameters.player_name; // Retrieve player_name from path parameters
-
-    const apiKey = event.headers['x-api-key'];
-    if (apiKey !== process.env.API_KEY) { // Validate the API key
-        return {
-            statusCode: 401,
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ message: "Unauthorized" })
-        };
-    }
-
-    const queryUrl = `https://g437e9ea50f2c14-leaderboard.adb.eu-frankfurt-1.oraclecloudapps.com/ords/admin/leaderboard/checkUsername/${encodeURIComponent(playerName)}`;
-
     try {
+        if (event.httpMethod !== 'GET') {
+            return {
+                statusCode: 405,
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ message: "Method Not Allowed" })
+            };
+        }
+
+        const playerName = event.pathParameters ? event.pathParameters.player_name : null;
+        if (!playerName) {
+            return {
+                statusCode: 400,
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ message: "Player name not provided" })
+            };
+        }
+
+        const apiKey = event.headers['x-api-key'];
+        if (apiKey !== process.env.API_KEY) {
+            return {
+                statusCode: 401,
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ message: "Unauthorized" })
+            };
+        }
+
+        const queryUrl = `https://g437e9ea50f2c14-leaderboard.adb.eu-frankfurt-1.oraclecloudapps.com/ords/admin/leaderboard/checkUsername/${encodeURIComponent(playerName)}`;
+
         const response = await axios.get(queryUrl, {
             headers: { 'Content-Type': 'application/json' }
         });
 
-        const exists = response.data.exists; // Assume the Oracle endpoint returns JSON with an 'exists' field
+        const exists = response.data.exists;
 
         return {
             statusCode: 200,
