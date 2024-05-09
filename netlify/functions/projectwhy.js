e695f0780680aa81353752d6709c12c1cfad3276
Doruk Şaşmaz <info@doruksasmaz.com>
2024-05-09 18:05:43 +0300

    Update projectwhy.js

--- netlify/functions/projectwhy.js
+++ netlify/functions/projectwhy.js
@@ -1,28 +1,41 @@
 const { Octokit } = require("@octokit/rest");
+const axios = require("axios");
 
 exports.handler = async function (event, context) {
-    const octokit = new Octokit({
-        auth: process.env.GITHUB_TOKEN
-    });
-
+    const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
     const gameId = event.queryStringParameters.gameId;
-    const repo = 'ProjectWhy'; // Replace with your repository name
-    const owner = 'Diabolical-Studios'; // Replace with your GitHub username or organization
+    const repo = 'ProjectWhy';  // Your repository name
+    const owner = 'Diabolical-Studios';  // Your GitHub username or organization
 
     try {
         const release = await octokit.repos.getLatestRelease({ owner, repo });
         const asset = release.data.assets.find(asset => asset.name === `Build-StandaloneWindows64.zip`);
 
         if (asset) {
+            const fileResponse = await axios({
+                url: asset.url,
+                method: 'GET',
+                responseType: 'stream',
+                headers: {
+                    'Authorization': `token ${process.env.GITHUB_TOKEN}`,
+                    'Accept': 'application/octet-stream'
+                }
+            });
+
             return {
                 statusCode: 200,
-                body: JSON.stringify({ url: asset.browser_download_url })
+                headers: {
+                    'Content-Type': 'application/zip',
+                    'Content-Disposition': `attachment; filename="${gameId}.zip"`
+                },
+                body: fileResponse.data.read().toString('base64'),
+                isBase64Encoded: true
             };
         } else {
             return { statusCode: 404, body: 'No release found for this game' };
         }
     } catch (error) {
-        console.error('Failed to fetch latest release:', error);
+        console.error('Failed to fetch or download release:', error);
         return { statusCode: 500, body: 'Internal Server Error' };
     }
 };
