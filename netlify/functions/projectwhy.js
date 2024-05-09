39cc6209eea7b0a616bbbc13ec13aba3d644e425
Doruk Şaşmaz <info@doruksasmaz.com>
2024-05-09 18:20:58 +0300

    Update projectwhy.js

--- netlify/functions/projectwhy.js
+++ netlify/functions/projectwhy.js
@@ -2,30 +2,32 @@ const { Octokit } = require("@octokit/rest");
 
 exports.handler = async function (event, context) {
     const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
-    const repo = 'ProjectWhy';  // Your repository name
-    const owner = 'Diabolical-Studios';  // Your GitHub username or organization
+    const repo = 'ProjectWhy';
+    const owner = 'Diabolical-Studios';
 
     try {
         const release = await octokit.repos.getLatestRelease({ owner, repo });
-        const asset = release.data.assets.find(asset => asset.name === `Build-StandaloneWindows64.zip`);
+        console.log("Release Data:", release.data);  // Log release data
+
+        const asset = release.data.assets.find(asset => asset.name === 'Build-StandaloneWindows64.zip');
+        console.log("Asset Found:", asset);  // Log found asset
 
         if (asset) {
-            // Get a temporary URL to download the asset
-            const downloadUrl = await octokit.repos.getReleaseAsset({
+            const { data: downloadData } = await octokit.request('GET /repos/{owner}/{repo}/releases/assets/{asset_id}', {
                 owner,
                 repo,
                 asset_id: asset.id,
                 headers: {
-                    accept: 'application/octet-stream'
+                    accept: 'application/octet-stream',
+                    Authorization: `token ${process.env.GITHUB_TOKEN}`
                 }
             });
 
+            console.log("Download URL:", downloadData.url);  // Log URL
+
             return {
-                statusCode: 302,  // HTTP status code for redirection
-                headers: {
-                    'Location': downloadUrl.data.url
-                },
-                body: JSON.stringify({ url: downloadUrl.data.url })
+                statusCode: 200,
+                body: JSON.stringify({ url: downloadData.url })
             };
         } else {
             return { statusCode: 404, body: 'No release found for this game' };
