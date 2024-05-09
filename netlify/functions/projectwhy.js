6e8195898c661ebc6b1f240be03bd0ae8e1f5f1a
Doruk Şaşmaz <info@doruksasmaz.com>
2024-05-09 18:27:00 +0300

    Update projectwhy.js

--- netlify/functions/projectwhy.js
+++ netlify/functions/projectwhy.js
@@ -7,27 +7,19 @@ exports.handler = async function (event, context) {
 
     try {
         const release = await octokit.repos.getLatestRelease({ owner, repo });
-        console.log("Release Data:", release.data);  // Log release data
-
         const asset = release.data.assets.find(asset => asset.name === 'Build-StandaloneWindows64.zip');
-        console.log("Asset Found:", asset);  // Log found asset
 
         if (asset) {
-            const { data: downloadData } = await octokit.request('GET /repos/{owner}/{repo}/releases/assets/{asset_id}', {
-                owner,
-                repo,
-                asset_id: asset.id,
-                headers: {
-                    accept: 'application/octet-stream',
-                    Authorization: `token ${process.env.GITHUB_TOKEN}`
-                }
-            });
+            // Log the URL to verify it's correct
+            console.log("Asset URL for download:", asset.browser_download_url);
 
-            console.log("Download URL:", downloadData.url);  // Log URL
-
+            // Redirect to the asset's browser download URL
             return {
-                statusCode: 200,
-                body: JSON.stringify({ url: downloadData.url })
+                statusCode: 302,  // Use HTTP status code for redirection
+                headers: {
+                    'Location': asset.browser_download_url
+                },
+                body: ''  // No need to return a body for redirection
             };
         } else {
             return { statusCode: 404, body: 'No release found for this game' };
