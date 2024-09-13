9e441d979b0b243501871bdcf61feda3193c6ac6
blazittx <info@doruksasmaz.com>
2024-09-13 19:53:41 +0200

    Small Fix

--- netlify/functions/getLeaderboard.js
+++ netlify/functions/getLeaderboard.js
@@ -21,7 +21,7 @@ exports.handler = async (event) => {
 
     try {
         // Modify the URL to pass the game_id as a query parameter to the Oracle endpoint
-        const response = await axios.get(`https://g437e9ea50f2c14-diabolicalleaderboards.adb.eu-frankfurt-1.oraclecloudapps.com/ords/admin/leaderboard/scores/?game_id=${encodeURIComponent(game_id)}`);
+        const response = await axios.get(`https://g437e9ea50f2c14-diabolicalleaderboards.adb.eu-frankfurt-1.oraclecloudapps.com/ords/admin/leaderboard/scores/${encodeURIComponent(game_id)}`);
 
         return {
             statusCode: 200,
